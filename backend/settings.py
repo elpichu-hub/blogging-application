cbc58380a939bff7985118939c813c637207f6b8
lazaro <lazlemlop@gmail.com>
2022-12-14 12:22:32 -0500

    fix aws

--- backend/settings.py
+++ backend/settings.py
@@ -225,7 +225,7 @@ django_heroku.settings(locals())
 
 #AWS
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY_ID']
 AWS_STORAGE_BUCKET_NAME = 'blogging-applications'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_FILE_OVERWRITE = False
