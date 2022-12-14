854faf984b9bc345f5d425813c565c5e65bf7fd7
lazaro <lazlemlop@gmail.com>
2022-12-14 13:41:17 -0500

    fix websocket url

--- frontend/src/httpEndPoint.js
+++ frontend/src/httpEndPoint.js
@@ -1,14 +1,11 @@
 const productionURL = 'https://blogging-applications.herokuapp.com';
 const developmentURL = 'http://127.0.0.1:8001';
 
-const createUrlForWebSocket = () => {
-    const splitString = endPoint.url.split('//')
-    return splitString[1];
-};
+
 
 const endPoint = {
     'url': productionURL,
-    'urlForWebSocket': createUrlForWebSocket()
+    'urlForWebSocket': 'blogging-applications.herokuapp.com'
 };
 
 
