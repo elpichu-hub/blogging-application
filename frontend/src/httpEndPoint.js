65039c0717c03225a34f6ddeb01a7f7f3aa750c4
lazaro <lazlemlop@gmail.com>
2022-12-14 13:14:16 -0500

    create function to edit url for websocket

--- frontend/src/httpEndPoint.js
+++ frontend/src/httpEndPoint.js
@@ -1,9 +1,14 @@
 const productionURL = 'https://blogging-applications.herokuapp.com';
 const developmentURL = 'http://127.0.0.1:8001';
 
+const createUrlForWebSocket = () => {
+    const splitString = endPoint.url.split('//')
+    return splitString.shift()
+};
 
 const endPoint = {
     'url': productionURL,
+    'urlForWebSocket': createUrlForWebSocket()
 };
 
 
