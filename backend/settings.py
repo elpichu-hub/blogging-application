5af161476b8e56ebf981b67159be7e13dd3886fa
elpichu-hub <80866847+elpichu-hub@users.noreply.github.com>
2023-08-26 21:29:45 -0400

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -228,8 +228,8 @@ DJOSER = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'lazarogonzalez.auto@gmail.com'
-EMAIL_HOST_PASSWORD = 'tstnzfqhhkzrjuxa'
+EMAIL_HOST_USER = EMAIL
+EMAIL_HOST_PASSWORD = PASSWORD
 
 django_heroku.settings(locals())
 
